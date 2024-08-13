3065c0da1e2ac8fc221550a6105060bd5bd0eff5
bioroid17 <trace1997@ajou.ac.kr>
2024-08-13 22:05:45 +0900

    FUNCTIONS - Generics Recap

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-// Generic을 통한 다형성
-type SuperPrint = {
-  <T>(arr: T[]): T;
-};
+type SuperPrint = <T, M>(a: T[], b: M) => T;
 
 const superPrint: SuperPrint = (arr) => arr[0];
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true]);
-const c = superPrint(["a", "b", "c"]);
-const d = superPrint([1, 2, true, false]);
+const a = superPrint([1, 2, 3, 4], "x");
+const b = superPrint([true, false, true], 1);
+const c = superPrint(["a", "b", "c"], false);
+const d = superPrint([1, 2, true, false], []);
+
+// d.toUppercase() // 만약 generic을 쓰지 않고 any를 쓰면 런타임 에러 발생
