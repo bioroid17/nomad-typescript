519b437002854c9d0ed6f6730736a9933930dbfe
bioroid17 <trace1997@ajou.ac.kr>
2024-08-13 13:35:37 +0900

    OVERVIEW OF TYPESCRIPT - Implicit Types vs Explicit Types

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-const player = {
-  age: 12,
-};
+let a = "hello";
+let b: boolean = false;
+let c: number[] = [];
+c.push(1);
 
-// 타입 에러
-player.age = false;
+const player = {
+  name: "nico",
+};
+// '{ name: string; }' 형식에 'hello' 속성이 없습니다.
+player.hello();
