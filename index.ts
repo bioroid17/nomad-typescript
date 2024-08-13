3ec19564b41974a024c0e87daadfb4f5010963c9
bioroid17 <trace1997@ajou.ac.kr>
2024-08-13 13:59:31 +0900

    FUNCTIONS - Call Signatures

--- index.ts
+++ index.ts
@@ -1,32 +1,4 @@
-// unknown 타입
-let a: unknown;
-// 타입 확인 필수
-if (typeof a === "number") {
-  let b = a + 1;
-}
-if (typeof a === "string") {
-  a.toUpperCase();
-}
+// call signature
+type Add = (a: number, b: number) => number;
 
-// void 타입: 아무것도 반환하지 않는 함수를 대상으로 사용
-// 굳이 void를 명시할 필요 없음
-function hello(): void {
-  console.log("x");
-}
-const b = hello();
-// b.toUpperCase() // void엔 toUpperCase()가 없으므로 에러
-
-// never: 함수가 절대 return 하지 않을 때 사용
-function hello2(): never {
-  throw new Error("xxx");
-}
-// else 부분은 never로, 절대 실행되어선 안 된다.
-function hello3(name: string | number) {
-  if (typeof name === "string") {
-    name;
-  } else if (typeof name === "number") {
-    name;
-  } else {
-    name;
-  }
-}
+const add: Add = (a, b) => a + b;
