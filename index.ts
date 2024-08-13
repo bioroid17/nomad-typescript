1b1008376b96c2c7b719f20df5b0609f2407a82e
bioroid17 <trace1997@ajou.ac.kr>
2024-08-13 15:58:36 +0900

    FUNCTIONS - Overloading

--- index.ts
+++ index.ts
@@ -1,4 +1,33 @@
-// call signature
-type Add = (a: number, b: number) => number;
+type Config = {
+  path: string;
+  state: object;
+};
 
-const add: Add = (a, b) => a + b;
+// 오버로딩(파라미터 타입 별)
+// call signature를 길게 쓸 수 있다. 오버로딩 때문
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") {
+    console.log(config);
+  } else {
+    console.log(config.path);
+  }
+};
+
+// 오버로딩(파라미터 개수 별)
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+// c는 optional이나 다름없다. 따라서 ?와 함께 타입을 표시해준다.
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add(1, 2);
+add(1, 2, 3);
