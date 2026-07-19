6d350802916a1a334170f9df106a3d5144823ea4
bioroid17 <trace1997@ajou.ac.kr>
2026-07-19 21:18:58 +0900

    TYPESCRIPT BLOCKCHAIN - Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { exit, init } from "./myPackage";
+import crypto from "crypto";
 
-init({ debug: true, url: "https://example.com" });
-exit(1);
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string,
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
