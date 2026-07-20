b426337db7e7af4728d130749603b09b755a245a
bioroid17 <trace1997@ajou.ac.kr>
2026-07-21 00:29:10 +0900

    TYPESCRIPT BLOCKCHAIN - Chain

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,40 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash(): string {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string): void {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data,
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks(): Block[] {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+// This will not affect the blockchain since we're working with a copy
+blockchain.getBlocks().push(new Block("fake hash", 111, "Hacked data!"));
+
+console.log(blockchain.getBlocks());
